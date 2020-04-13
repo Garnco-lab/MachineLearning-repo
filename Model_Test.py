8b7d5b52c5255b484250777e5195d0aaa80839c6
Steven Deleon <stevendeleonwork@gmail.com>
2020-04-12 17:01:27 -0700

    updated picture to show prediction

--- Model_Test.py
+++ Model_Test.py
@@ -2,7 +2,8 @@ from keras.datasets import cifar10
 import keras.utils as utils
 from keras.models import load_model
 import numpy as np
-
+import matplotlib.pyplot as plt
+supernum = 2
 labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 (_, _), (x_test, y_test) = cifar10.load_data()
@@ -18,8 +19,11 @@ model = load_model("Image_Classifier.h5")
 # print("Test loss:", results[0])
 # print("Test accuracy:", results[1])
 
-test_image_data = np.asarray([x_test[0]])
+
+test_image_data = np.asarray([x_test[supernum]])
 
 prediction = model.predict(x=test_image_data)
 max_index = np.argmax(prediction[0])
 print("Prediction:", labels[max_index])
+plt.imshow(x_test[supernum])
+plt.show()
